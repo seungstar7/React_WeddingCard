dd00277b997d5b77b6ad87d32a48f67f811da891
seungstar7 <seungstar7@users.noreply.github.com>
2023-12-05 06:39:30 +0000

    deploy: 924bda1e43df8308075014e89b612cd4d5b1deba

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
