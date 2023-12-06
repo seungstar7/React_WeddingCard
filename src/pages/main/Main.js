4ce18cf138ab93681c2ba0edfc135559f8c98bf6
kim <shkim0922@gmail.com>
2023-12-06 09:34:40 +0900

    hooks 동작테스트

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 // import {Navigate, useNavigate} from 'react-router'
 const Main = () => {
+    const [ testBtn, setTestBtn ] = useState(1);
 
     // const navigate = useNavigate();
     const fnNavi = ( e ) => {
         // navigate(`/${e.target.name}` )
 
     }
+
+    const btnTest = () => {
+        setTestBtn(testBtn + 1);
+    }
+
+    useEffect(()=>{
+        console.log("react working")
+    },[])
     return(
         <>
-            <button name={"snake"} onClick={fnNavi}>test1</button>
+            <p>{testBtn}</p>
+            <button name={"snake"} onClick={btnTest}>test1</button>
             <button name={"matgo"} onClick={fnNavi}>test2</button>
         </>
     )
