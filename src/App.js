10ae5f15af104c873f49bfbe6354b8aad374662f
kim <shkim0922@gmail.com>
2023-12-15 14:43:39 +0900

    라우터 TEST

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "/assets/css/public.css";
 import "/assets/css/button.css";
 import ScrollComponent from '@/compnents/cardMain/ScrollComponent'
 import CardMain from '@/compnents/cardMain/CardMain'
-// import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
     const [ menu , setMenu ] = useState('intro');
     const menuDef = ( menuType ) => {
@@ -13,17 +13,17 @@ function App() {
 
     return (
         <>
-            {menu == 'intro' && <ScrollComponent menuDef={menuDef}/>}
-            {menu == 'main' && <CardMain/>}
+            {/*{menu == 'intro' && <ScrollComponent menuDef={menuDef}/>}*/}
+            {/*{menu == 'main' && <CardMain/>}*/}
 
-            {/*<BrowserRouter>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/" element={<ScrollComponent/>} ></Route>*/}
-            {/*        <Route path="/main" element={<CardMain/>} ></Route>*/}
-            {/*    </Routes>*/}
-            {/*</BrowserRouter>*/}
-            {/*<ScrollComponent/>*/}
-            {/*<CardMain/>*/}
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Routes>
+                    <Route path="/" element={<ScrollComponent/>} ></Route>
+                    <Route path="/main" element={<CardMain/>} ></Route>
+                </Routes>
+            </BrowserRouter>
+            <ScrollComponent/>
+            <CardMain/>
         </>
     );
 }
