36e5fe22ac3b3cb45a48a05201f9a7c780cfc5f2
kim <shkim0922@gmail.com>
2023-12-21 18:00:49 +0900

    번역 수정

--- src/compnents/main/Contact/childComponents/parentContactChildComponent/BrideParentContact.js
+++ src/compnents/main/Contact/childComponents/parentContactChildComponent/BrideParentContact.js
@@ -7,7 +7,7 @@ const BrideParentContact = () => {
         <>
             {/*신부측 혼주 영역 시작*/}
             <div data-v-0470861c="" data-v-4752b0dc="" className="parents-contact">
-                <span data-v-0470861c="" className="title">{t("")}신부측 혼주</span>
+                <span data-v-0470861c="" className="title">{t("신부측 혼주")}</span>
                 <div data-v-0470861c="" father="[object Object]" mother="[object Object]"
                      className="parents-wrap">
                     <div data-v-39c2c16b="" data-v-0470861c="" className="parent">
